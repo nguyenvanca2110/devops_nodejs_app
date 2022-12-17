acb3580016de434c15df6306342ce9809f5eb651
Kakashi <nguyenvanca2110@gmail.com>
2022-12-18 02:11:45 +0900

    Edit name of Container

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello Kakashi" }');
+    res.send('{ "response": "Hello Kakashi. It is new Jenkins " }');
 });
 
 app.get('/will', function (req, res) {
